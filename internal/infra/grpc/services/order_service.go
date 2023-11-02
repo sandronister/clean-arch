e8fe6784ef88c88a156912376a18b40f58045f6d
Julio Sandroni <sandronister@gmail.com>
2023-11-02 09:26:25 -0300

    grpc service

---
+++ internal/infra/grpc/services/order_service.go
@@ -0,0 +1,40 @@
+package services
+
+import (
+	"context"
+
+	"github.com/sandronister/clean-arch/internal/infra/grpc/pb"
+	"github.com/sandronister/clean-arch/internal/usecase"
+)
+
+type OrderService struct {
+	pb.UnimplementedOrderServiceServer
+	CreateOrderUseCase usecase.CreateOrderUseCase
+}
+
+func NewOrderService(createOrderUseCase usecase.CreateOrderUseCase) *OrderService {
+	return &OrderService{
+		CreateOrderUseCase: createOrderUseCase,
+	}
+}
+
+func (s *OrderService) CreaterOrder(ctx context.Context, in *pb.CreateOrderRequest) (*pb.CreateOrderResponse, error) {
+	dto := usecase.OrderInputDTO{
+		ID:    in.Id,
+		Price: float64(in.Price),
+		Tax:   float64(in.Tax),
+	}
+
+	output, err := s.CreateOrderUseCase.Execute(dto)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.CreateOrderResponse{
+		Id:         output.ID,
+		Price:      float32(output.Price),
+		Tax:        float32(output.Tax),
+		FinalPrice: float32(output.FinalPrice),
+	}, nil
+}
