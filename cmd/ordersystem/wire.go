1331436000d0a34b91e2b3ff5fe5992514cd86e5
Julio Sandroni <sandronister@gmail.com>
2023-11-25 20:18:06 -0300

    chore: start implement list order use case

--- cmd/ordersystem/wire.go
+++ cmd/ordersystem/wire.go
@@ -42,6 +42,14 @@ func NewCreateOrderUseCase(db *sql.DB, eventDispatcher events.EventDispatcherInt
 	return &usecase.CreateOrderUseCase{}
 }
 
+func NewListOrderUseCase(db *sql.DB) *usecase.ListOrderUseCase {
+	wire.Build(
+		setOrderRepositoryDependency,
+	)
+
+	return &usecase.ListOrderUseCase{}
+}
+
 func NewWebOrderHandler(db *sql.DB, eventDispatcher events.EventDispatcherInterface) *web.WebOrderHandler {
 	wire.Build(
 		setOrderRepositoryDependency,
