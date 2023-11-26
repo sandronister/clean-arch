4311e0db5b185607546f70757b8b6e7083b3b3d8
Julio Sandroni <sandronister@gmail.com>
2023-11-25 21:44:26 -0300

    chore:implement list webserver

--- internal/infra/web/order_handler.go
+++ internal/infra/web/order_handler.go
@@ -50,3 +50,20 @@ func (h *WebOrderHandler) Create(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
+
+func (h *WebOrderHandler) List(w http.ResponseWriter, r *http.Request) {
+	listOrder := usecase.NewListOrderUseCase(h.OrderRepository)
+
+	output, err := listOrder.Execute()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	err = json.NewEncoder(w).Encode(output)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+}
--- internal/infra/web/webserver/webserver.go
+++ internal/infra/web/webserver/webserver.go
@@ -7,28 +7,41 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+type HandleType struct {
+	Method string
+	Handle http.HandlerFunc
+}
+
 type WebServer struct {
 	Router        chi.Router
-	Handlers      map[string]http.HandlerFunc
+	Handlers      map[string]HandleType
 	WebServerPort string
 }
 
 func NewWebServer(serverPort string) *WebServer {
 	return &WebServer{
 		Router:        chi.NewRouter(),
-		Handlers:      make(map[string]http.HandlerFunc),
+		Handlers:      make(map[string]HandleType),
 		WebServerPort: serverPort,
 	}
 }
 
-func (s *WebServer) AddHandlers(path string, handle http.HandlerFunc) {
-	s.Handlers[path] = handle
+func (s *WebServer) AddHandlers(path string, method string, handle http.HandlerFunc) {
+	s.Handlers[path] = HandleType{Method: method, Handle: handle}
 }
 
 func (s *WebServer) Start() {
 	s.Router.Use(middleware.Logger)
 	for path, handler := range s.Handlers {
-		s.Router.Handle(path, handler)
+
+		if handler.Method == http.MethodPost {
+			s.Router.Post(path, handler.Handle)
+		}
+
+		if handler.Method == http.MethodGet {
+			s.Router.Get(path, handler.Handle)
+		}
+
 	}
 	http.ListenAndServe(s.WebServerPort, s.Router)
 }
